f7c9d2bafe8ceef38ed7a004a55005bc48d55d57
Reverdan Almeida Sparinger <reverdan@gmail.com>
2026-09-09 20:30:45 -0300

    Correcao

--- CrudPessoasNoite/CrudPessoasNoite/apresentacao/frmPrincipal.xaml.cs
+++ CrudPessoasNoite/CrudPessoasNoite/apresentacao/frmPrincipal.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CrudPessoasNoite.DAL;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -7,20 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CrudPessoasNoite
+namespace CrudPessoasNoite.apresentacao
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica interna para frmPrincipal.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class frmPrincipal : Window
     {
-        public MainWindow()
+        public frmPrincipal()
         {
             InitializeComponent();
-            
         }
     }
 }
